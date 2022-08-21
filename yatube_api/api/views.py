1532d414d662f8f94534aa78a00a869ada0e34ef
Artem Sinitsyn <artemxpma@gmail.com>
2022-08-21 19:12:41 +0600

    readme

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post
+from posts.models import Comment, Follow, Group, Post
 from rest_framework import viewsets
 
-from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 from .permissions import AuthorOrReadOnly
 
 
@@ -32,3 +32,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    permission_classes = (AuthorOrReadOnly,)
+    serializer_class = FollowSerializer
